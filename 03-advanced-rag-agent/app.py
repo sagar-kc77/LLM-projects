219ea967c1c1c9edea09d8e3a23fb77f5a0ea812
Sagar <161312727+sagar-kc77@users.noreply.github.com>
2024-08-21 20:04:39 +0545

    Update app.py

--- 03-advanced-rag-agent/app.py
+++ 03-advanced-rag-agent/app.py
@@ -67,6 +67,52 @@ async def init():
 
 cl.on_message
 
+async def process_response(res):
+    # retrieve the retrieval chain initialized for the current session
+    chain = cl.user_session.get("chain") 
+    # Chinlit callback handler
+    cb = cl.AsyncLangchainCallbackHandler(
+                                        stream_final_answer=True, 
+                                        answer_prefix_tokens=["FINAL", "ANSWER"]
+                                        )
+    cb.answer_reached = True
+    print("in retrieval QA")
+    res = await chain.acall(res, callbacks=[cb])
+    print(f"response: {res}")
+    answer = res["result"]
+    sources = res["source_documents"]
+    source_elements = []
 
+    # Get the metadata and texts from the user session
+    metadatas = cl.user_session.get("metadatas")
+    all_sources = [m["source"] for m in metadatas]
+    texts = cl.user_session.get("texts")
+
+    if sources:
+        found_sources = []
+
+        # Add the sources to the message
+        for source in sources:
+            print(source.metadata)
+            try :
+                source_name = source.metadata["source"]
+            except :
+                source_name = ""
+            # Get the index of the source
+            text = source.page_content
+            found_sources.append(source_name)
+            # Create the text element referenced in the message
+            source_elements.append(cl.Text(content=text, name=source_name))
+
+        if found_sources:
+            answer += f"\nSources: {', '.join(found_sources)}"
+        else:
+            answer += "\nNo sources found"
+
+    if cb.has_streamed_final_answer:
+        cb.final_stream.elements = source_elements
+        await cb.final_stream.update()
+    else:
+        await cl.Message(content=answer, elements=source_elements).send()
 
 # chainlit run app.py
